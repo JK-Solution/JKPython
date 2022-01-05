3ebf912b375cfd90654af95593932c7cf1e9ae13
Jack <zh224635@163.com>
2022-01-05 15:06:47 +0800

    add gitee remote

---
+++ day3/str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   str.py
+@Time    :   2022/01/05 14:21:27
+@Author  :   Zhang Hong 
+@Version :   1.0
+@Contact :   zh224635@163.com
+@License :   (C)Copyright 2021-2022, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+# here put the import lib
+
+str1 = 'Hello World!'
+str2 = "hello 张宏！"
+print('声明字符串。。。')
+print(str1)
+print(str2)
